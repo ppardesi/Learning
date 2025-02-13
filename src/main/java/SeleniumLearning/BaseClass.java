d63e2ce0539dab58559c3ccb709b1158b5cc31f2
Paras Pardesi <paraspardesi@gmail.com>
2025-02-13 08:19:47 +0530

    Adding new files with parameter variable
    Add the files

--- src/main/java/SeleniumLearning/BaseClass.java
+++ src/main/java/SeleniumLearning/BaseClass.java
@@ -4,16 +4,18 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 @Test
 public class BaseClass {
 
 	public WebDriver driver;
-	//Locators of the page
+	// Locators of the page
 	By username = By.id("inputUsername");
 	By password = By.name("inputPassword");
 	By forgotYourPassword = By.linkText("Forgot your password?");
@@ -23,9 +25,16 @@ public class BaseClass {
 	By incorrectUN = By.xpath("//p[contains(text(),'Incorrect username')]");
 	By forgotPasswordText = By.xpath("//h2");
 
+	@Parameters("Browser")
 	@BeforeTest
-	public void startInstance() {
-		driver = new ChromeDriver();
+	public void startInstance(String browserName) {
+		System.out.println("Browser Name passed: " + browserName);
+		if (browserName.equals("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+
 		driver.manage().window().fullscreen();
 	}
 
@@ -34,7 +43,7 @@ public class BaseClass {
 
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(username).sendKeys("rahul");
-		driver.findElement(password).sendKeys("rahul"); //rahulshettyacademy
+		driver.findElement(password).sendKeys("rahul"); // rahulshettyacademy
 		driver.findElement(signInButton).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.findElement(incorrectUN).getText();
@@ -42,11 +51,10 @@ public class BaseClass {
 			Assert.assertEquals(driver.findElement(incorrectUN).getText(), "* Incorrect username or password");
 		}
 		driver.findElement(forgotYourPassword).click();
-		if(driver.findElement(forgotPasswordText).getText().equalsIgnoreCase("Forgot password")){
+		if (driver.findElement(forgotPasswordText).getText().equalsIgnoreCase("Forgot password")) {
 			Assert.assertEquals(driver.findElement(forgotPasswordText).getText(), "Forgot password");
 		}
-		
-		
+
 	}
 
 	@AfterSuite
